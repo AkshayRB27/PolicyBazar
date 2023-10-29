7ca1a4032de74e3134f97f472cc6bed763d2716b
Abhishek <Abhishek@192.168.1.42>
2023-10-29 14:14:02 +0530

    [Akshay]dataProvider_policyBazaar/Myaccount_PC.java:[3,34]

--- src/dataProvider_policyBazaar/Myaccount_PC.java
+++ src/dataProvider_policyBazaar/Myaccount_PC.java
@@ -1,6 +1,6 @@
 package dataProvider_policyBazaar;
 
-import org.apache.commons.logging.Log;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/pomClassUingTestNG/Myaccount_PC.java
+++ src/pomClassUingTestNG/Myaccount_PC.java
@@ -1,6 +1,6 @@
 package pomClassUingTestNG;
 
-import org.apache.commons.logging.Log;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
